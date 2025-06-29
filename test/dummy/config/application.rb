96ef975f494e80af8347e944ae836b98c1a46fcf
Jim Gay <jim@saturnflyer.com>
2025-06-29 13:31:14 -0400

    Remove unused active_record

--- test/dummy/config/application.rb
+++ test/dummy/config/application.rb
@@ -8,7 +8,6 @@ require "rails"
 require "action_controller/railtie"
 require "action_view/railtie"
 require "active_model/railtie"
-require "active_record/railtie"
 require "action_mailer/railtie"
 require "rails/test_unit/railtie"
 
